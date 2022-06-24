8cd3d344e1fa2330c4973f44cf229435e9992cae
Filip Vrba <filipvrba@pm.me>
2022-06-24 13:05:46 +0200

    change architexture to fv

--- src/app.rb
+++ src/app.rb
@@ -24,33 +24,9 @@ File.open(file) do |d|
   data = d.readlines
 end
 
-# find default words
-# e.c. def add to list
-# find uses def
 fv = FV.new
-
-data.each_with_index do |row, i|
-  FV::WORDS.each do |key, value|
-    if row.include?(value)
-      fv.words.append Word.new(value, row, i)
-      break
-    end
-  end
-
-  fv.words.each do |word|
-    name = word.get_name
-    if name
-      if row.include?(name) &&
-        !row.include?(word.to_s)
-        word.childs.append Word.new(name, row, i)
-        break
-      end
-    end
-  end
-end
+fv.find_words(data)
 
 fv.words.each do |word|
-  word.childs.each do |child|
-    p child
-  end
+  p word
 end
--- src/fv.rb
+++ src/fv.rb
@@ -11,4 +11,26 @@ class FV
   def initialize()
     @words = Array.new
   end
+
+  def find_words(data)
+    data.each_with_index do |row, i|
+      FV::WORDS.each do |key, value|
+        if row.include?(value)
+          @words.append Word.new(value, row, i)
+          break
+        end
+      end
+  
+      @words.each do |word|
+        name = word.get_name
+        if name
+          if row.include?(name) &&
+            !row.include?(word.to_s)
+            word.childs.append Word.new(name, row, i)
+            break
+          end
+        end
+      end
+    end
+  end
 end
